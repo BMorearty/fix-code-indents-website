8431ebcc06384c536806f5e252fe27098064060a
Brian Morearty <brian@morearty.org>
2023-02-08 12:05:54 -0800

    Add terms and conditions.

--- src/routes/terms/+page.svelte
+++ src/routes/terms/+page.svelte
@@ -1,3 +1,113 @@
 <h1>Terms of Service</h1>
 
-<p>What should I put here?</p>
+<div class="page page-1">
+  <h2>1. Introduction</h2>
+  <p>
+    These Terms And Conditions (these “Terms” or these “Terms And Conditions”) contained herein on
+    this webpage, shall govern your use of the Tabs to Spaces extension for Google Docs (a.k.a.
+    “Tabs to Spaces” or “The Software”). These Terms apply in full force and effect to your use of
+    this Software and by using this Software, you expressly accept all terms and conditions
+    contained herein in full. You must not use this Software, if you have any objection to any of
+    these Software Standard Terms And Conditions.
+  </p>
+  <p>
+    This Software is not for use by any minors (defined as those who are not at least 18 years of
+    age), and you must not use this Software if you a minor.
+  </p>
+  <h2>2. Intellectual Property Rights</h2>
+  <p>
+    Other than content you own, which you may have opted to include on this Software, under these
+    Terms, <span class="token">Brian Morearty</span> and/or his licensors own all rights to the intellectual
+    property and material contained in this Software, and all such rights are reserved. You are granted
+    a limited license only, subject to the restrictions provided in these Terms, for purposes of viewing
+    the material contained on this Software.
+  </p>
+</div>
+<div class="page page-2">
+  <h2>3. Restrictions</h2>
+  <p>You are expressly and emphatically restricted from all of the following:</p>
+  <ol>
+    <li>using this Software in any way that is, or may be, damaging to this Software;</li>
+    <li>using this Software in any way that impacts user access to this Software;</li>
+    <li>
+      using this Software contrary to applicable laws and regulations, or in a way that causes, or
+      may cause, harm to the Software, or to any person or business entity;
+    </li>
+    <li>
+      engaging in any data mining, data harvesting, data extracting or any other similar activity in
+      relation to this Software, or while using this Software;
+    </li>
+    <li>using this Software to engage in any advertising or marketing;</li>
+  </ol>
+  <p>
+    Certain areas of this Software are restricted from access by you and <span class="token"
+      >Brian Morearty</span
+    > may further restrict access by you to any areas of this Software, at any time, in his sole and
+    absolute discretion. Any user ID and password you may have for this Software are confidential and
+    you must maintain confidentiality of such information.
+  </p>
+</div>
+<div class="page page-3">
+  <h2>4. Your Content</h2>
+  <p>N/A.</p>
+  <h2>5. No warranties</h2>
+  <p>
+    This Software is provided “as is,” with all faults, and <span class="token">Brian Morearty</span
+    > makes no express or implied representations or warranties, of any kind related to this Software
+    or the materials contained on this Software. Additionally, nothing contained on this Software shall
+    be construed as providing consult or advice to you.
+  </p>
+  <h2>6. Limitation of liability</h2>
+  <p>
+    In no event shall <span class="token">Brian Morearty</span>, nor any of his officers, directors
+    and employees, be liable to you for anything arising out of or in any way connected with your
+    use of this Software, whether such liability is under contract, tort or otherwise, and
+    <span class="token">Brian Morearty</span>, including his officers, directors and employees shall
+    not be liable for any indirect, consequential or special liability arising out of or in any way
+    related to your use of this Software.
+  </p>
+</div>
+<div class="page page-4">
+  <h2>7. Indemnification</h2>
+  <p>
+    You hereby indemnify to the fullest extent <span class="token">Brian Morearty</span> from and against
+    any and all liabilities, costs, demands, causes of action, damages and expenses (including reasonable
+    attorney’s fees) arising out of or in any way related to your breach of any of the provisions of
+    these Terms.
+  </p>
+  <h2>8. Severability</h2>
+  <p>
+    If any provision of these Terms is found to be unenforceable or invalid under any applicable
+    law, such unenforceability or invalidity shall not render these Terms unenforceable or invalid
+    as a whole, and such provisions shall be deleted without affecting the remaining provisions
+    herein.
+  </p>
+  <h2>9. Variation of Terms</h2>
+  <p>
+    <span class="token">Brian Morearty</span> is permitted to revise these Terms at any time as it sees
+    fit, and by using this Software you are expected to review such Terms on a regular basis to ensure
+    you understand all terms and conditions governing use of this Software.
+  </p>
+  <h2>10. Assignment</h2>
+  <p>
+    <span class="token">Brian Morearty</span> shall be permitted to assign, transfer, and subcontract
+    his rights and/or obligations under these Terms without any notification or consent required. However,
+    you shall not be permitted to assign, transfer, or subcontract any of your rights and/or obligations
+    under these Terms.
+  </p>
+</div>
+<div class="page page-5">
+  <h2>11. Entire Agreement</h2>
+  <p>
+    These Terms, including any legal notices and disclaimers contained on this Software, constitute
+    the entire agreement between <span class="token">Brian Morearty</span> and you in relation to your
+    use of this Software, and supersede all prior agreements and understandings with respect to the same.
+  </p>
+  <h2>12. Governing Law &amp; Jurisdiction</h2>
+  <p>
+    These Terms will be governed by and construed in accordance with the laws of the State of <span
+      class="token">California</span
+    >, and you submit to the non-exclusive jurisdiction of the state and federal courts located in
+    <span class="token">California</span> for the resolution of any disputes.
+  </p>
+</div>
