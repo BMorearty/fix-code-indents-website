f4dd3a598926d03776d43d130c625c643ee8e7c8
Brian Morearty <brian@morearty.org>
2023-02-14 20:49:57 -0800

    Server-side length checks.

--- src/routes/feedback/+page.server.ts
+++ src/routes/feedback/+page.server.ts
@@ -4,13 +4,18 @@ import { fail } from '@sveltejs/kit';
 import type { ServerLoad } from '@sveltejs/kit';
 import { SUPPORT_EMAIL_NAME, SUPPORT_EMAIL_ADDRESS } from '$env/static/private';
 
+const MAX_NAME = 50;
+const MAX_EMAIL = 100;
+const MAX_SUBJ = 150;
+const MAX_BODY = 4000;
+
 export const load: ServerLoad = () => {
   return {
     maxLength: {
-      name: 50,
-      email: 100,
-      subject: 150,
-      body: 4000,
+      name: MAX_NAME,
+      email: MAX_EMAIL,
+      subject: MAX_SUBJ,
+      body: MAX_BODY,
     },
   };
 };
@@ -26,14 +31,14 @@ export const actions = {
       return fail(400, { error: 'Please specify name, email, subject, and body.' });
     }
     await sendEmail({
-      fromEmail: fromEmail as string,
+      fromEmail: (fromEmail as string).slice(0, MAX_EMAIL),
       fromName: 'Fix Code Indents Mailer',
-      replyToEmail: fromEmail as string,
-      replyToName: fromName as string,
+      replyToEmail: (fromEmail as string).slice(0, MAX_EMAIL),
+      replyToName: (fromName as string).slice(0, MAX_NAME),
       toEmail: SUPPORT_EMAIL_ADDRESS,
       toName: SUPPORT_EMAIL_NAME,
-      subject: `Fix Code Indents feedback: ${subject}`,
-      body: body as string,
+      subject: `Fix Code Indents feedback: ${subject.slice(0, MAX_SUBJ)}`,
+      body: (body as string).slice(MAX_BODY),
     });
     return {
       success: 'Message sent successfully.',
