05386c23a031d470735e569ef1dc26f7889660a5
Brian Morearty <brian@morearty.org>
2023-02-18 22:49:57 -0800

    Just use Venmo.

--- src/routes/payment/+page.svelte
+++ src/routes/payment/+page.svelte
@@ -1,9 +1,5 @@
 <h1>Payment</h1>
 <p>Is Fix Code Indents saving you time? Would you mind paying for it as a thank-you?</p>
 <p>Pay me whatever you think is fair.</p>
-<p>Here are two ways you can pay:</p>
-<ol>
-  <li><a href="https://account.venmo.com/u/BMorearty">Venmo me at @BMorearty.</a></li>
-  <li><a href="https://www.buymeacoffee.com/bmorearty">Send me money on Buy Me a Coffee.</a></li>
-</ol>
+<p><a href="https://account.venmo.com/u/BMorearty">Venmo me at @BMorearty.</a></p>
 <p>Thanks!</p>
