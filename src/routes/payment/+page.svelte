135de3ec9bdc7dcb944cdafee547f87200f23d01
Brian Morearty <brian@morearty.org>
2023-02-19 17:13:59 -0800

    wording

--- src/routes/payment/+page.svelte
+++ src/routes/payment/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Payment</h1>
 <p>Is Fix Code Indents saving you time? Would you mind paying for it as a thank-you?</p>
-<p>Pay me whatever you think is fair.</p>
+<p>Pay me whatever you think is fair for the time it saves you.</p>
 <p><a href="https://account.venmo.com/u/BMorearty">Venmo me at @BMorearty.</a></p>
 <p>Thanks!</p>
